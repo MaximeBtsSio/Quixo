0a468317bfa59ffdd538aa095eaa3b31c48c212e
MaximeSaliou <maxime.saliou@bts-malraux.net>
2015-03-11 11:16:46 +0100

    Quixo

---
+++ Quixo.pro
@@ -0,0 +1,25 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-03-04T11:59:01
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Quixo
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES += \
+    Image.qrc
+
+OTHER_FILES +=
---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QPixmap>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+
+{
+    ui->setupUi(this);
+    QPixmap * mon_image = new QPixmap ("/chat.jpg");
+    QGraphicsPixmapItem * mon_item = new QGraphicsPixmapItem (* mon_image);
+    QGraphicsScene * ma_scene = new QGraphicsScene (ui->ma_vue);
+    ma_scene->addItem(mon_item);
+    ui->ma_vue->setScene(ma_scene);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
